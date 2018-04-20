cb526755e0a5bc4484ae63b568c0d8735326e7d0
phgoff <mous17zy@hotmail.com>
2018-04-20 11:16:53 +0700

    Initial Commit

---
+++ app/src/main/java/com/phirathat/day5_android/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.phirathat.day5_android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("result","Outout - Result");
+        setResult(Activity.RESULT_OK,returnIntent);
+        finish();
+
+    }
+}
